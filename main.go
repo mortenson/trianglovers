b6b080257ca606b48d9e12379270c8aa883ae495
Sam Mortenson <samuel@mortenson.coffee>
2020-05-02 12:04:19 -0700

    Proof of concepting

--- main.go
+++ main.go
@@ -39,16 +39,20 @@ type trianglover struct {
 
 var trianglovers []*trianglover
 
-func drawPolygon(screen *ebiten.Image, clr color.Color, coordinates ...int) {
+func drawPolygon(screen *ebiten.Image, clr color.Color, coordinates [][2]int) {
 	image, _ := ebiten.NewImage(16, 16, ebiten.FilterDefault)
 	image.Fill(clr)
 	triopts := &ebiten.DrawTrianglesOptions{}
 	vertices := make([]ebiten.Vertex, 0)
 	indices := make([]uint16, 0)
-	for i := 0; i < len(coordinates)-1; i += 2 {
+	totalX := 0
+	totalY := 0
+	for i := 0; i < len(coordinates); i++ {
+		totalX += coordinates[i][0]
+		totalY += coordinates[i][1]
 		vertices = append(vertices, ebiten.Vertex{
-			DstX:   float32(coordinates[i]),
-			DstY:   float32(coordinates[i+1]),
+			DstX:   float32(coordinates[i][0]),
+			DstY:   float32(coordinates[i][1]),
 			SrcX:   0,
 			SrcY:   0,
 			ColorR: 1,
@@ -56,23 +60,17 @@ func drawPolygon(screen *ebiten.Image, clr color.Color, coordinates ...int) {
 			ColorB: 1,
 			ColorA: 1,
 		})
-		indices = append(indices, uint16(i/2))
+		indices = append(indices, uint16(i))
 		if i > 0 {
-			indices = append(indices, uint16(len(coordinates)/2))
-			indices = append(indices, uint16(i/2))
+			indices = append(indices, uint16(len(coordinates)))
+			indices = append(indices, uint16(i))
 		}
 	}
 	indices = append(indices, 0)
-	indices = append(indices, uint16(len(coordinates)/2))
+	indices = append(indices, uint16(len(coordinates)))
 
-	totalX := 0
-	totalY := 0
-	for i := 0; i < len(coordinates)-1; i += 2 {
-		totalX += coordinates[i]
-		totalY += coordinates[i+1]
-	}
-	centerX := totalX / (len(coordinates) / 2)
-	centerY := totalY / (len(coordinates) / 2)
+	centerX := totalX / len(coordinates)
+	centerY := totalY / len(coordinates)
 	vertices = append(vertices, ebiten.Vertex{
 		DstX:   float32(centerX),
 		DstY:   float32(centerY),
@@ -102,8 +100,38 @@ func update(screen *ebiten.Image) error {
 		}
 	}
 
-	drawPolygon(screen, color.RGBA{255, 0, 0, 255}, 50, 0, 100, 50, 100, 100, 50, 150, 0, 100, 0, 50)
-	//drawPolygon(screen, color.RGBA{255, 0, 0, 255}, 100, 100, 150, 150, 50, 150)
+	hexPoints := [][2]int{
+		{50, 0},
+		{100, 25},
+		{100, 75},
+		{50, 100},
+		{0, 75},
+		{0, 25},
+	}
+	drawPolygon(screen, color.RGBA{255, 0, 0, 255}, hexPoints)
+	a := 10
+	b := 40
+	c := 75
+	points := make([][2]int, 0)
+	for i := 0; i < len(hexPoints); i++ {
+		nextIndex := i + 1
+		if nextIndex >= len(hexPoints) {
+			nextIndex = 0
+		}
+		xDelta := (hexPoints[nextIndex][0] - hexPoints[i][0]) / 17
+		yDelta := (hexPoints[nextIndex][1] - hexPoints[i][1]) / 17
+		for j := 0; j < 17; j++ {
+			points = append(points, [2]int{
+				hexPoints[i][0] + j*xDelta,
+				hexPoints[i][1] + j*yDelta,
+			})
+		}
+	}
+	drawPolygon(screen, color.White, [][2]int{
+		points[a],
+		points[b],
+		points[c],
+	})
 
 	return nil
 }
