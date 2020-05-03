3f7c37c5725e1668480e36b60fd00aaba5ae90e4
Sam Mortenson <samuel@mortenson.coffee>
2020-05-03 06:51:46 -0700

    Refactored drawMatchChart

--- main.go
+++ main.go
@@ -33,16 +33,14 @@ type preferencePoints struct {
 }
 
 type trianglover struct {
-	name   string
-	points preferencePoints
+	name        string
+	points      preferencePoints
+	guessPoints preferencePoints
 }
 
 var trianglovers []*trianglover
 
 func drawPolygon(screen *ebiten.Image, clr color.Color, coordinates [][2]int) {
-	image, _ := ebiten.NewImage(16, 16, ebiten.FilterDefault)
-	image.Fill(clr)
-	triopts := &ebiten.DrawTrianglesOptions{}
 	vertices := make([]ebiten.Vertex, 0)
 	indices := make([]uint16, 0)
 	totalX := 0
@@ -82,7 +80,42 @@ func drawPolygon(screen *ebiten.Image, clr color.Color, coordinates [][2]int) {
 		ColorA: 1,
 	})
 
+	image, _ := ebiten.NewImage(16, 16, ebiten.FilterDefault)
+	image.Fill(clr)
+	triopts := &ebiten.DrawTrianglesOptions{}
 	screen.DrawTriangles(vertices, indices, image, triopts)
+}
+
+func drawMatchChart(screen *ebiten.Image, x, y int, prefPoints preferencePoints) {
+	hexPoints := [][2]int{
+		{50 + x, 0 + y},
+		{100 + x, 25 + y},
+		{100 + x, 75 + y},
+		{50 + x, 100 + y},
+		{0 + x, 75 + y},
+		{0 + x, 25 + y},
+	}
+	drawPolygon(screen, color.RGBA{255, 0, 0, 255}, hexPoints)
+	points := make([][2]int, 0)
+	for i := 0; i < len(hexPoints); i++ {
+		nextIndex := i + 1
+		if nextIndex >= len(hexPoints) {
+			nextIndex = 0
+		}
+		xDelta := (hexPoints[nextIndex][0] - hexPoints[i][0]) / 17
+		yDelta := (hexPoints[nextIndex][1] - hexPoints[i][1]) / 17
+		for j := 0; j < 17; j++ {
+			points = append(points, [2]int{
+				hexPoints[i][0] + j*xDelta,
+				hexPoints[i][1] + j*yDelta,
+			})
+		}
+	}
+	drawPolygon(screen, color.White, [][2]int{
+		points[prefPoints.a],
+		points[prefPoints.b],
+		points[prefPoints.c],
+	})
 }
 
 func update(screen *ebiten.Image) error {
@@ -100,38 +133,9 @@ func update(screen *ebiten.Image) error {
 		}
 	}
 
-	hexPoints := [][2]int{
-		{50, 0},
-		{100, 25},
-		{100, 75},
-		{50, 100},
-		{0, 75},
-		{0, 25},
+	for i, lover := range trianglovers {
+		drawMatchChart(screen, i*100, 0, lover.points)
 	}
-	drawPolygon(screen, color.RGBA{255, 0, 0, 255}, hexPoints)
-	a := 10
-	b := 40
-	c := 75
-	points := make([][2]int, 0)
-	for i := 0; i < len(hexPoints); i++ {
-		nextIndex := i + 1
-		if nextIndex >= len(hexPoints) {
-			nextIndex = 0
-		}
-		xDelta := (hexPoints[nextIndex][0] - hexPoints[i][0]) / 17
-		yDelta := (hexPoints[nextIndex][1] - hexPoints[i][1]) / 17
-		for j := 0; j < 17; j++ {
-			points = append(points, [2]int{
-				hexPoints[i][0] + j*xDelta,
-				hexPoints[i][1] + j*yDelta,
-			})
-		}
-	}
-	drawPolygon(screen, color.White, [][2]int{
-		points[a],
-		points[b],
-		points[c],
-	})
 
 	return nil
 }
