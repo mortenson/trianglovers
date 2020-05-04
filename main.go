fa3fc22ed003552e43ab5fd297bf03903d36ccc8
Sam Mortenson <samuel@mortenson.coffee>
2020-05-03 22:10:56 -0700

    Refactor points into array

--- main.go
+++ main.go
@@ -22,32 +22,17 @@ const (
 	height = 600
 )
 
-type gameMode int
-
-const (
-	modeTitle gameMode = iota
-	modeTutorial
-	modeQuestion
-)
-
-type gameState struct {
-	mode gameMode
-}
-
-type preferencePoints struct {
-	a int
-	b int
-	c int
-}
+type vertex [2]int
 
 type trianglover struct {
 	name        string
-	points      preferencePoints
-	guessPoints preferencePoints
+	headPoint   int
+	points      [3]int
+	guessPoints [3]int
 }
 
 type dragPoint struct {
-	position [2]int
+	position vertex
 	dragging bool
 }
 
@@ -61,11 +46,11 @@ var hexLabels = []string{
 	"Romance",
 	"Family",
 }
-var dragPoints map[string]*dragPoint
-var dragTargets [][2]int
+var dragPoints [3]*dragPoint
+var dragTargets []vertex
 var currentLover *trianglover
 
-func drawPolygon(screen *ebiten.Image, clr color.Color, coordinates [][2]int) {
+func drawPolygon(screen *ebiten.Image, clr color.Color, coordinates []vertex) {
 	path := vector.Path{}
 	path.MoveTo(float32(coordinates[0][0]), float32(coordinates[0][1]))
 	for i := 1; i < len(coordinates); i++ {
@@ -77,13 +62,13 @@ func drawPolygon(screen *ebiten.Image, clr color.Color, coordinates [][2]int) {
 	})
 }
 
-func distance(p1, p2 [2]int) float64 {
+func distance(p1, p2 vertex) float64 {
 	first := math.Pow(float64(p2[0]-p1[0]), 2)
 	second := math.Pow(float64(p2[1]-p1[1]), 2)
 	return math.Sqrt(first + second)
 }
 
-func angle(p1, p2, p3 [2]int) float64 {
+func angle(p1, p2, p3 vertex) float64 {
 	radians := math.Atan2(float64(p3[1]-p1[1]), float64(p3[0]-p1[0])) - math.Atan2(float64(p2[1]-p1[1]), float64(p2[0]-p1[0]))
 	degrees := radians * 180 / math.Pi
 	if degrees > 0 {
@@ -93,9 +78,8 @@ func angle(p1, p2, p3 [2]int) float64 {
 	}
 }
 
-func drawMatchChart(screen *ebiten.Image, x, y int, prefPoints preferencePoints) {
-	// Draw hexagon.
-	hexPoints := [][2]int{
+func getHexPoints(x, y int) []vertex {
+	return []vertex{
 		{50 + x, 0 + y},
 		{100 + x, 25 + y},
 		{100 + x, 75 + y},
@@ -103,9 +87,11 @@ func drawMatchChart(screen *ebiten.Image, x, y int, prefPoints preferencePoints)
 		{0 + x, 75 + y},
 		{0 + x, 25 + y},
 	}
-	drawPolygon(screen, color.RGBA{255, 0, 0, 255}, hexPoints)
+}
+
+func getHexBoundaryPoints(hexPoints []vertex) []vertex {
 	// Segment hexagon boundary into 102 points.
-	points := make([][2]int, 0)
+	points := make([]vertex, 0)
 	for i := 0; i < len(hexPoints); i++ {
 		nextIndex := i + 1
 		if nextIndex >= len(hexPoints) {
@@ -114,24 +100,32 @@ func drawMatchChart(screen *ebiten.Image, x, y int, prefPoints preferencePoints)
 		xDelta := float64(hexPoints[nextIndex][0]-hexPoints[i][0]) / 17
 		yDelta := float64(hexPoints[nextIndex][1]-hexPoints[i][1]) / 17
 		for j := 0.0; j < 17; j++ {
-			points = append(points, [2]int{
+			points = append(points, vertex{
 				hexPoints[i][0] + int(j*xDelta),
 				hexPoints[i][1] + int(j*yDelta),
 			})
 		}
 	}
+	return points
+}
+
+func drawMatchChart(screen *ebiten.Image, x, y int, prefPoints [3]int) {
+	// Draw hexagon.
+	hexPoints := getHexPoints(x, y)
+	drawPolygon(screen, color.RGBA{255, 0, 0, 255}, hexPoints)
+	points := getHexBoundaryPoints(hexPoints)
 	// Draw the triangle.
-	drawPolygon(screen, color.White, [][2]int{
-		points[prefPoints.a],
-		points[prefPoints.b],
-		points[prefPoints.c],
+	drawPolygon(screen, color.White, []vertex{
+		points[prefPoints[0]],
+		points[prefPoints[1]],
+		points[prefPoints[2]],
 	})
 	// Register drag points (targets)
 	dragTargets = points
 	// Register drag points.
-	dragPoints["a"].position = points[prefPoints.a]
-	dragPoints["b"].position = points[prefPoints.b]
-	dragPoints["c"].position = points[prefPoints.c]
+	dragPoints[0].position = points[prefPoints[0]]
+	dragPoints[1].position = points[prefPoints[1]]
+	dragPoints[2].position = points[prefPoints[2]]
 	// Add labels.
 	for i, hexLabel := range hexLabels {
 		x := hexPoints[i][0]
@@ -150,56 +144,18 @@ func drawMatchChart(screen *ebiten.Image, x, y int, prefPoints preferencePoints)
 		}
 		text.Draw(screen, hexLabel, defaultFont, x, y, color.White)
 	}
-	// Add angles.
-	a := points[prefPoints.a]
-	b := points[prefPoints.b]
-	c := points[prefPoints.c]
+	// Add angles (@todo).
+	a := points[prefPoints[0]]
+	b := points[prefPoints[1]]
+	c := points[prefPoints[2]]
 	A := angle(a, b, c)
 	B := angle(b, c, a)
 	C := angle(c, a, b)
-	text.Draw(screen, fmt.Sprintf("%+v %+v %+v", points[prefPoints.a], points[prefPoints.b], points[prefPoints.c]), defaultFont, 12, 12, color.White)
+	text.Draw(screen, fmt.Sprintf("%+v %+v %+v", points[prefPoints[0]], points[prefPoints[1]], points[prefPoints[2]]), defaultFont, 12, 12, color.White)
 	text.Draw(screen, fmt.Sprintf("A: %f B: %f C: %f", A, B, C), defaultFont, 12, 24, color.White)
 }
 
-func drawTrianglover(screen *ebiten.Image, lover *trianglover) {
-
-}
-
-func init() {
-	tt, err := truetype.Parse(fonts.MPlus1pRegular_ttf)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defaultFont = truetype.NewFace(tt, &truetype.Options{
-		Size:    12,
-		DPI:     72,
-		Hinting: font.HintingFull,
-	})
-	dragPoints = map[string]*dragPoint{
-		"a": {},
-		"b": {},
-		"c": {},
-	}
-	dragTargets = [][2]int{}
-}
-
-func update(screen *ebiten.Image) error {
-	ebiten.SetScreenScale(1.5)
-	if trianglovers == nil {
-		trianglovers = make([]*trianglover, 0)
-		for i := 0; i < 10; i++ {
-			trianglovers = append(trianglovers, &trianglover{
-				name: fmt.Sprintf("%d", i),
-				points: preferencePoints{
-					a: rand.Intn(34),
-					b: rand.Intn(34) + 34,
-					c: rand.Intn(34) + 68,
-				},
-			})
-		}
-		currentLover = trianglovers[0]
-	}
-
+func handleDrag() {
 	mouseX, mouseY := ebiten.CursorPosition()
 	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) || inpututil.IsMouseButtonJustReleased(ebiten.MouseButtonLeft) {
 		for _, point := range dragPoints {
@@ -208,7 +164,7 @@ func update(screen *ebiten.Image) error {
 	}
 	if inpututil.IsMouseButtonJustPressed(ebiten.MouseButtonLeft) {
 		for _, point := range dragPoints {
-			if distance(point.position, [2]int{mouseX, mouseY}) < 5 {
+			if distance(point.position, vertex{mouseX, mouseY}) < 5 {
 				point.dragging = true
 				break
 			}
@@ -221,17 +177,17 @@ func update(screen *ebiten.Image) error {
 		min := 0
 		max := 34
 		switch pointID {
-		case "b":
+		case 1:
 			min = 34
 			max = 68
-		case "c":
+		case 2:
 			min = 68
 			max = 102
 		}
 		closestID := -1
 		closestDistance := 100.0
 		for i := min; i < max; i++ {
-			d := distance(dragTargets[i], [2]int{mouseX, mouseY})
+			d := distance(dragTargets[i], vertex{mouseX, mouseY})
 			if d < closestDistance {
 				closestID = i
 				closestDistance = d
@@ -240,16 +196,65 @@ func update(screen *ebiten.Image) error {
 		if closestID == -1 {
 			continue
 		}
-		switch pointID {
-		case "a":
-			currentLover.points.a = closestID
-		case "b":
-			currentLover.points.b = closestID
-		case "c":
-			currentLover.points.c = closestID
+		currentLover.points[pointID] = closestID
+	}
+}
+
+func drawTrianglover(screen *ebiten.Image, lover *trianglover) {
+	points := getHexBoundaryPoints(getHexPoints(100, 100))
+	vertices := []vertex{points[lover.points[0]], points[lover.points[1]], points[lover.points[2]]}
+	centerX := (vertices[0][0] + vertices[1][0] + vertices[2][0]) / 3
+	centerY := (vertices[0][0] + vertices[1][0] + vertices[2][0]) / 3
+	// Scale triangle up.
+	scale := 2
+	for i := range vertices {
+		vertices[i][0] = centerX + ((vertices[i][0] - centerX) * scale)
+		vertices[i][1] = centerY + ((vertices[i][1] - centerY) * scale)
+	}
+	// Rotate triangle.
+	for i := range vertices {
+		if i == lover.headPoint {
+			vertices[i] = points[0]
+			continue
 		}
+		vertices[i] = points[int(math.Abs(float64(lover.points[i]-lover.points[lover.headPoint])))]
+	}
+	drawPolygon(screen, color.White, vertices)
+}
+
+func init() {
+	tt, err := truetype.Parse(fonts.MPlus1pRegular_ttf)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defaultFont = truetype.NewFace(tt, &truetype.Options{
+		Size:    12,
+		DPI:     72,
+		Hinting: font.HintingFull,
+	})
+	dragTargets = []vertex{}
+	dragPoints = [3]*dragPoint{{}, {}, {}}
+}
+
+func update(screen *ebiten.Image) error {
+	ebiten.SetScreenScale(1.5)
+	if trianglovers == nil {
+		trianglovers = make([]*trianglover, 0)
+		for i := 0; i < 10; i++ {
+			trianglovers = append(trianglovers, &trianglover{
+				name: fmt.Sprintf("%d", i),
+				points: [3]int{
+					rand.Intn(34),
+					rand.Intn(34) + 34,
+					rand.Intn(34) + 68,
+				},
+				headPoint: rand.Intn(3),
+			})
+		}
+		currentLover = trianglovers[0]
 	}
 
+	handleDrag()
 	drawMatchChart(screen, width-180, height-120, currentLover.points)
 	drawTrianglover(screen, currentLover)
 
